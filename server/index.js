c981c4cf436af28e644bd9a5365a5f8300450e3a
pumpedlunch <88350639+pumpedlunch@users.noreply.github.com>
2022-04-29 13:16:11 -0700

    starting out

--- server/index.js
+++ server/index.js
@@ -8,12 +8,38 @@ const port = 3042;
 app.use(cors());
 app.use(express.json());
 
+<<<<<<< Updated upstream
 const balances = {
   "1": 100,
   "2": 50,
   "3": 75,
 }
 
+=======
+
+//generate 3 public keys with balances
+const balances = {};
+
+for(let i = 1; i < 4; i++) {
+  //generate public key in hex
+
+  const privateKey = secp.utils.randomPrivateKey();
+  const publicKey = formatPublicKeyToETH(secp.utils.bytesToHex(secp.getPublicKey(privateKey)));
+
+  //save address and arbitrary balance in balances
+  balances[`${publicKey}`] = i * 50;
+
+  //console log out account info
+  console.log(`Account #${i}: \n Public Key: ${publicKey} \n Private Key: ${privateKey} \n Balance: ${balances[`${publicKey}`]}`)
+}
+
+console.log(balances);
+
+function formatPublicKeyToETH(publicKey) {
+  return `0x${publicKey.substring((publicKey.length - 41), (publicKey.length -1))}`
+}
+
+>>>>>>> Stashed changes
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
   const balance = balances[address] || 0;
@@ -21,7 +47,15 @@ app.get('/balance/:address', (req, res) => {
 });
 
 app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
+  const {sender, recipient, amount, senderPrivateKey} = req.body;
+  if(formatPublicKeyToETH(secp.getPublicKey(senderPrivateKey)) !== sender) {
+    console.log('invalid private key');
+    return
+  }
+  if(balances[sender] < amount) {
+    console.log('insufficient funds')
+    return
+  }
   balances[sender] -= amount;
   balances[recipient] = (balances[recipient] || 0) + +amount;
   res.send({ balance: balances[sender] });
